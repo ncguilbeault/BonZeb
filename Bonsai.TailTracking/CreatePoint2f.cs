445cd8467690141106f7471f8b003a3ae1235012
ncguilbeault <nicholas.guilbeault@mail.utoronto.ca>
2019-05-15 18:11:48 -0400

    Added functions for dynamically cropping around fish. Also added function to initialize a Point2f variable.

---
+++ Bonsai.TailTracking/CreatePoint2f.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using OpenCV.Net;
+
+namespace Bonsai.TailTracking
+{
+    public class CreatePoint2f : Source<Point2f>
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public override IObservable<Point2f> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(new Point2f(X, Y)));
+        }
+    }
+}
---
+++ Bonsai.TailTracking/CropImageAroundFish.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using OpenCV.Net;
+
+namespace Bonsai.TailTracking
+{
+    public class CropImageAroundFish : Transform<IplImage, IplImage>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public override IObservable<IplImage> Process(IObservable<IplImage> source)
+        {
+            return source.Select(input =>
+            {
+                Rect rect = new Rect(X, Y, Width, Height);
+                if (rect.Width > 0 && rect.Height > 0)
+                {
+                    return input.GetSubRect(rect);
+                }
+
+                return input;
+            });
+        }
+    }
+}
