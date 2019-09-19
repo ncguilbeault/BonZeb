d87fe63c60517f4314cd99c5627dcaec04675865
Nicholas Guilbeault <nicholas.guilbeault@mail.utoronto.ca>
2019-09-18 20:26:03 -0400

    Added functions to convert radians into degrees and degrees into radians.

---
+++ Bonsai.TailTracking/ConvertRadiansToDegrees.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Bonsai.TailTracking
+{
+
+    [Description("Converts radians to degrees.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+
+    public class ConvertRadiansToDegrees : Transform<double, double>
+    {
+        public override IObservable<double> Process(IObservable<double> source)
+        {
+            return source.Select(value => Utilities.ConvertRadiansToDegrees(value));
+        }
+    }
+}
