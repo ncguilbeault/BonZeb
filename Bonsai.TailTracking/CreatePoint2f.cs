b0292be8c160539bf370713cbda86ffa446d79b2
Nicholas Guilbeault <nicholas.guilbeault@mail.utoronto.ca>
2019-09-20 17:13:04 -0400

    Updated CreatePoint2f function to allow values to change on the fly.

--- Bonsai.TailTracking/CreatePoint2f.cs
+++ Bonsai.TailTracking/CreatePoint2f.cs
@@ -11,15 +11,29 @@ namespace Bonsai.TailTracking
 
     public class CreatePoint2f : Source<Point2f>
     {
+
+        private Point2f point = new Point2f();
+        event Action<Point2f> ValueChanged;
+
+        void OnValueChanged(Point2f value)
+        {
+            ValueChanged?.Invoke(value);
+        }
+
         [Description("X value to use for the point2f variable.")]
-        public float X { get; set; }
+        public float X { get => point.X; set { point.X = value; OnValueChanged(point); } }
 
         [Description("Y value to use for the point2f variable.")]
-        public float Y { get; set; }
+        public float Y { get => point.Y; set { point.Y = value; OnValueChanged(point); } }
 
         public override IObservable<Point2f> Generate()
         {
-            return Observable.Defer(() => Observable.Return(new Point2f(X, Y)));
+            return Observable.Defer(() => Observable.Return(point)).Concat(Observable.FromEvent<Point2f>(handler => ValueChanged += handler, handler => ValueChanged -= handler));
+        }
+
+        public IObservable<Point2f> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(x => point);
         }
     }
 }
