75271e8e07e59f948c0aebe878c370cdd9f955e1
Nicholas Guilbeault <nicholas.guilbeault@mail.utoronto.ca>
2019-10-06 13:25:21 -0400

    Added CalculateMeanTailCurvature and CalculateCumulativeTailCurvature functions.

--- Bonsai.TailTracking/CalculateCumulativeTailCurvature.cs
+++ Bonsai.TailTracking/CalculateCumulativeTailCurvature.cs
@@ -9,11 +9,11 @@ namespace Bonsai.TailTracking
     [Description("Calculates the tail curvature by rotating an array of points around the angle between the first two points and calculating the angle between each pair of remaining points.")]
     [WorkflowElementCategory(ElementCategory.Transform)]
 
-    public class CalculateMeanTailCurvature : Transform<double[], double>
+    public class CalculateCumulativeTailCurvature : Transform<double[], double>
     {
         public override IObservable<double> Process(IObservable<double[]> source)
         {
-            return source.Select(value => Utilities.CalculateMean(value));
+            return source.Select(value => Utilities.CalculateSum(value));
         }
     }
 }
